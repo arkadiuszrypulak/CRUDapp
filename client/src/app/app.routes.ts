7e749a8d810664b8775678371479317d6a552cc6
Arkadiusz Rypulak <83660682+arkadiuszrypulak@users.noreply.github.com>
2024-02-14 13:32:43 +0100

    Create auth guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuestionsListComponent } from './components/questions-list/questions-list.component';
 import { AddQuestionsComponent } from './components/add-questions/add-questions.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'questions', pathMatch: 'full' },
   { path: 'questions', component: QuestionsListComponent },
-  { path: 'add', component: AddQuestionsComponent },
+  { path: 'signin', component: LoginComponent },
+  { path: 'add', component: AddQuestionsComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
